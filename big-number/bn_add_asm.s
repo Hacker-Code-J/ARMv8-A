511d7bf1d78688176fafae939dd47565e8d8bdae
Hacker-Code-J-RaspberryPi <hacker3740@gmail.com>
2024-11-14 17:54:52 +0900

    RPi241114v1.1

--- big-number/bn_add_asm.s
+++ big-number/bn_add_asm.s
@@ -1,6 +1,15 @@
 .global bn_add_asm
 .type bn_add_asm, %function
 
+// void bn_add_asm(uint64_t* res, const uint64_t* op1, const uint64_t* op2, int32_t n);
+// x0: res (Address)
+// x1: op1 (Address)
+// x2: op2 (Address)
+// x3: n
+// x4: *res (Contents)
+// x5: *op1 (Contents)
+// x6: *op2 (Contents)
+// x7: counter
 bn_add_asm:
         mov     w7, wzr
 
@@ -23,4 +32,4 @@ END_LOOP:
         adc     w0, wzr, wzr
 
         ret
-.size bn_add_asm, (. - bn_add_asm)
+        .size bn_add_asm, (. - bn_add_asm)
--- big-number/main.c
+++ big-number/main.c
@@ -1,19 +1,40 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <inttypes.h>
 
-extern void bn_add_asm(uint64_t *res, const uint64_t *op1, const uint64_t *op2, int n);
+extern void bn_add_asm(uint64_t *res, const uint64_t *op1, const uint64_t *op2, int32_t n);
+
+void bn_add_c(uint64_t *res, const uint64_t *op1, const uint64_t *op2, int32_t n) {
+    uint64_t carry = 0;
+    uint64_t temp = 0;
+
+    for (int i = 0; i < n; i++) {
+        temp = op1[i];
+        temp += carry;
+        carry = (temp < carry);
+        temp += op2[i];
+        carry += (temp < op2[i]);
+        res[i] = temp;
+    }
+}
 
 int main() {
     uint64_t op1[] = {1, 2, 3};
-    uint64_t op2[] = {4, 5, 6};
-    uint64_t res[3];
+    uint64_t op2[] = {4, 0xFFFFFFFFFFFFFFFF, 6};
+    uint64_t res_asm[3];
+    uint64_t res_c[3];
     int n = sizeof(op1) / sizeof(op1[0]);
 
-    bn_add_asm(res, op1, op2, n);
+    bn_add_asm(res_asm, op1, op2, n);
+    bn_add_c(res_c, op1, op2, n);
 
     for (int i = 0; i < n; i++) {
-        printf("Result[%d] = %" PRIu64 "\n", i, res[i]);
-    }
+        printf("0x%" PRIx64 ":", i, res_asm[i]);
+    } puts("");
+
+    for (int i = 0; i < n; i++) {
+        printf("0x%" PRIx64 ":", i, res_c[i]);
+    } puts("");
 
     return 0;
 }
