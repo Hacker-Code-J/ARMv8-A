f5d9471d24c86dd69f954757172715437c67cf64
Hacker-Code-J-RaspberryPi <hacker3740@gmail.com>
2024-11-16 00:17:22 +0900

    RPi241116v1

--- big-number/main.c
+++ big-number/main.c
@@ -19,8 +19,8 @@ void bn_add_c(uint64_t *res, const uint64_t *op1, const uint64_t *op2, int32_t n
 }
 
 int main() {
-    uint64_t op1[] = {1, 2, 3};
-    uint64_t op2[] = {4, 0xFFFFFFFFFFFFFFFF, 6};
+    uint64_t op1[3] = {0xFFFFFFFFFFFFFFFF, 0x99aabbccddeeff00, 0x1122334455667788};
+    uint64_t op2[3] = {0x0000000000000001, 0x1122334455667788, 0x99aabbccddeeff00};
     uint64_t res_asm[3];
     uint64_t res_c[3];
     int n = sizeof(op1) / sizeof(op1[0]);
@@ -28,12 +28,17 @@ int main() {
     bn_add_asm(res_asm, op1, op2, n);
     bn_add_c(res_c, op1, op2, n);
 
+    // printf("%02x %02x %02x\n", op1[0], op1[1], op1[2]);
+    // printf("%02x %02x %02x\n", op2[0], op2[1], op2[2]);
+    // printf("%02x %02x %02x\n", res_asm[0], res_asm[1], res_asm[2]);
+    // printf("%02x %02x %02x\n", res_c[0], res_c[1], res_c[2]);
+
     for (int i = 0; i < n; i++) {
-        printf("0x%" PRIx64 ":", i, res_asm[i]);
+        printf("0x%016" PRIx64 ":", res_asm[i]);
     } puts("");
 
     for (int i = 0; i < n; i++) {
-        printf("0x%" PRIx64 ":", i, res_c[i]);
+        printf("0x%016" PRIx64 ":", res_c[i]);
     } puts("");
 
     return 0;
